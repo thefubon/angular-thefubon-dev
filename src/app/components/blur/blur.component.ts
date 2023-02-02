7c2e31c9bf343b8dd0cf96471d2ac761a786bf7e
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2023-02-02 19:04:39 +0300

    upd

---
+++ src/app/components/blur/blur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-blur',
+  templateUrl: './blur.component.html',
+  styleUrls: ['./blur.component.css']
+})
+export class BlurComponent {
+  title = 'Hello World';
+}
