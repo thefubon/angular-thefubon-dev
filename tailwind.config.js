943a59f2eb626acb0f4765cf3e2ea7fa237e275d
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2023-02-02 19:09:59 +0300

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        xs: '1rem',
+        sm: '1rem',
+        lg: '1rem',
+        xl: '1rem',
+      },
+    },
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        default: "#2b2a35",
+        primary: '#FCD34D',
+      },
+      spacing: {
+        13: '3.25rem',
+        15: '3.75rem',
+        128: '32rem',
+        144: '36rem',
+      },
+    },
   },
   plugins: [],
 }
